ecd5571f72a18554e25ada59a7eff7e0b338fb13
Aylin Castillo <aylin.castillo@usach.cl>
2024-11-05 19:12:14 -0300

    cambios

--- trabajo-tingeso/frontend/src/http-common.js
+++ trabajo-tingeso/frontend/src/http-common.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const tingesoBackendServer = import.meta.env.VITE_TINGESO_BACKEND_SERVER;
+//const tingesoBackendServer = import.meta.env.VITE_TINGESO_BACKEND_SERVER;
 const tingesoBackendPort = import.meta.env.VITE_TINGESO_BACKEND_PORT;
 
-console.log("tingesoBackendServer: ", tingesoBackendServer);
+//console.log("tingesoBackendServer: ", tingesoBackendServer);
 console.log("tingesoBackendPort: ", tingesoBackendPort);
 
 export default axios.create({
-    baseURL: `http://${tingesoBackendServer}:${tingesoBackendPort}`,
+    baseURL: `http://104.41.29.47:${tingesoBackendPort}`,
     headers: {
         'Content-Type': 'application/json'
     }
